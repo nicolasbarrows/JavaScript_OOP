29b1d1207adf2138855aa96f6adb04f99e153b2b
Nicolas Barrows <contact@nicolasbarrows.com>
2023-09-25 17:32:23 -0500

    Exercise 1 COMPLETE

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
 console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function exercise(n) {
+    console.log(`EXERCISE ${n}:\n==========\n`);
+}
+
+exercise(1);
+
+class Person {
+    constructor(name, pets, hobbies) {
+        this.name = name;
+        this.pets = pets;
+        this.hobbies = hobbies;
+    } 
+    addHobby(hobby) {
+        this.hobbies.push(hobby);
+    }
+    removeHobby(hobby) {
+        this.hobbies.forEach((word) => {
+            if (word == hobby) {
+                this.hobbies.splice(this.hobbies.indexOf(word), 1);
+                this.oldHobby = word;
+            }
+            })
+    }
+    greeting() {
+        console.log(`Hello, ${this.name}. I am Computer. How is your pet, ${this.pets}?`);
+    }
+}
+
+let nicolas = new Person("Nicolas", "Kevi", ["Dungeons & Dragons", "Board Games"]);
+nicolas.addHobby("Woodworking");
+nicolas.removeHobby("Board Games");
+
+console.log(nicolas.greeting()); 
+
+exercise(2);
+
+class Coder extends Person {
+
+}
